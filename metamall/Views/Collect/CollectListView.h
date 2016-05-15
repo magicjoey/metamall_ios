21a0eb22e271254ae399c24af946735143c10125
magicjoey <out_lier@126.com>
2016-05-15 12:14:04 +0800

    commit some code

---
+++ metamall/Views/Collect/CollectListView.h
@@ -0,0 +1,12 @@
+//
+// Author: Magic Joey
+// Created: 2016-05-15 10:46,CollectListView.h V1.0
+// Description:  
+// Copyright (c) 2016 MetaStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CollectListView : NSObject
+@end
---
+++ metamall/Views/Collect/CollectListView.m
@@ -0,0 +1,14 @@
+//
+// Author: Magic Joey
+// Created: 2016-05-15 10:46,CollectListView.m V1.0
+// Description:  
+// Copyright (c) 2016 MetaStudio. All rights reserved.
+//
+
+#import "CollectListView.h"
+
+
+@implementation CollectListView {
+
+}
+@end
