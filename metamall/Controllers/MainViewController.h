a2bb61456220d7de5aa90605a2e9af2e9889d797
magicjoey <out_lier@126.com>
2016-05-17 23:30:56 +0800

    commit some code

---
+++ metamall/Controllers/MainViewController.h
@@ -0,0 +1,15 @@
+//
+// Author: Magic Joey
+// Created: 2016-05-17 23:27,MainViewController.h V1.0
+// Description:  
+// Copyright (c) 2016 MetaStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *players;
+
+@end
---
+++ metamall/Controllers/MainViewController.m
@@ -0,0 +1,14 @@
+//
+// Author: Magic Joey
+// Created: 2016-05-17 23:27,MainViewController.m V1.0
+// Description:  
+// Copyright (c) 2016 MetaStudio. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+
+@implementation MainViewController {
+
+}
+@end
